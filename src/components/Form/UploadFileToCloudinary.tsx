901a4b8570301951e25f42e38689040fb77193b6
ThuTrang21 <thut15794@gmail.com>
2025-04-15 22:13:10 +0700

    update

--- src/components/Form/UploadFileToCloudinary.tsx
+++ src/components/Form/UploadFileToCloudinary.tsx
@@ -2,11 +2,12 @@ const upload_preset="tim_viec"
 const cloud_name="thutrang21"
 const api_url =`https://api.cloudinary.com/v1_1/${cloud_name}/raw/upload`
 
-export const uploadImageToCloud=async(file:any)=>{
+export const uploadFileToCloud=async(file:any)=>{
     const data =new FormData();
     data.append("file",file);
     data.append("upload_preset",upload_preset);
     data.append("cloud_name",cloud_name);
+    data.append("folder", "timviec_file"); 
     const res=await fetch(api_url,{
         method:"post",
         body:data
